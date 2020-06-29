2682ecf749a31b8d32e02c724ff9db83e9d1de1c
TravisEntner <Travisentner@gmail.com>
2020-06-29 21:57:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#count = 0
+#statment = input("Enter a statment ")
+#guess = input("type a letter to find in your statment ")
+#for letter in statment:
+#  if letter == guess:
+#    count += 1
+#print(count, "letters found")
+
+test = "Pyton Programing"
+print("String: ", test)
+print("First character: ", test[:1])
+
+print("Last character: ", test[-1])
